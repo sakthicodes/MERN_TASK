fbf50381516503fad3d1ca5f18c2e7d8630caa9d
sakthicodes <sakthikumaran14122002@gmail.com>
2025-03-04 13:49:29 +0530

    update

--- fe/src/api/api.js
+++ fe/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api"; 
- 
+const API_URL = import.meta.env.VITE_API_URL; 
+
 const getAccessToken = () => localStorage.getItem("accessToken");
 const getRefreshToken = () => localStorage.getItem("refreshToken");
 const setAccessToken = (token) => localStorage.setItem("accessToken", token);
