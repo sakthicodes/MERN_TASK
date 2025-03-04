7248a482e4f8cad58f32715daf132db45784be2e
sakthicodes <sakthikumaran14122002@gmail.com>
2025-03-04 14:19:14 +0530

    first commit

--- be/server.js
+++ be/server.js
@@ -10,23 +10,24 @@ import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
- 
-app.use(express.json());
- 
 const corsOptions = {
-  origin: "https://mern-task-vercel.app",
+  origin: ["https://mern-task-vercel.app"], 
   methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
-
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // Handle preflight requests properly
+
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
- 
-mongoose.connect(process.env.MONGO_URI)
+
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     console.log("Connected to MongoDB");
